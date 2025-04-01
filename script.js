1e946745801f8dd42f80b67ce5652002f29933a9
mwanzia <shem.masoo@student.moringaschool.com>
2025-04-02 01:13:15 +0300

    13th  commit

--- script.js
+++ script.js
@@ -22,3 +22,54 @@ pageTurnBtn.forEach((el,index) => {
         }
     }
 })
+
+
+//contact me button when clicked
+
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me')
+
+contactMeBtn.onclick = () => {
+    pages.forEach((page, index) =>{
+        setTimeout(() => {
+            page.classList.add('turn');
+
+            setTimeout(() =>{
+                page.style.zIndex = 20 + index;
+            },500);
+        },(index +1)*200+100)
+    }
+)
+}
+
+//create reverse index function
+
+let totalPages =pages.length;
+let pageNumber =0;
+
+function reverseIndex() {
+    pageNumber--;
+    if(pageNumber < 0) {
+        pageNumber = totalPages - 1;
+    }
+}
+
+
+//back profile button when clicked
+
+const backProfileBtn = document.querySelector('.back-profile')
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) =>{
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(() =>{
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            },500);
+        },( index + 1)*200+100)
+    }
+)
+}
