26f4f3b85b282c5c4a5ebf9d95b2fde445152498
mwanzia <shem.masoo@student.moringaschool.com>
2025-04-02 00:49:56 +0300

    12th  commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//turn pages when click next or prev button
+
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el,index) => {
+
+    el.onclick =() => {
+        const pageTurnid = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnid);
+
+        if(pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() =>{
+                pageTurn.style.zIndex = 20 -index;
+            },500);
+        }
+        else{
+            pageTurn.classList.add('turn');
+            setTimeout(() =>{
+                pageTurn.style.zIndex = 20 + index;
+            },500);
+        }
+    }
+})
